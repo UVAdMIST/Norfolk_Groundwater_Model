c88f2f2dc7d09fe8a985a034389c837f0ff976c1
bdb3m <bdb3m@virginia.edu>
2018-07-05 10:15:31 -0400

    Pull forecasts for date range

--- Preprocess/HRRR_Archive_single_variable.py
+++ Preprocess/HRRR_Archive_single_variable.py
@@ -10,7 +10,7 @@ Steps:
 """
 
 import re
-from datetime import date
+from datetime import date, timedelta
 import os
 import sys
 import shutil
@@ -24,8 +24,9 @@ import numpy as np
 # =============================================================================
 #     Modify these
 # =============================================================================
-DATE = date(2016, 9, 6)    # Model run date YYYY,MM,DD
-hour = 20                # Model initialization hour
+sDATE = date(2016, 9, 30)  # Model run date YYYY,MM,DD
+eDATE = date(2016, 10, 1)
+hours = range(24)          # Model initialization hour
 fxx = range(19)            # Forecast hour range
                            # Note: Valid Time is the Date and Hour plus fxx.
 
@@ -38,126 +39,129 @@ var_to_match = 'APCP'      # must be part of a line in the .idx file
 # =============================================================================
 # =============================================================================
 
-# create directories to store data
-direct = 'C:/HRRR/HRRR_Archive_'+DATE.strftime('%Y%m%d')+'_hr_'+str(hour)
-os.makedirs(direct)
-os.makedirs(direct+"/GRIB2")
-os.makedirs(direct+"/TIF")
+days = (eDATE-sDATE).days
+DATES = [sDATE + timedelta(days=d) for d in range(days)]
 
-rainfall_data = np.zeros((19,8))
-shp_filename = 'C:/Users/Ben Bowes/Documents/HRSD GIS/Shallow Wells/Norfolk_Wells_UTM.shp'
+for DATE in DATES:
+    for hour in hours:
+        # create directories to store data
+        direct = 'C:/HRRR/HRRR_Archive_'+DATE.strftime('%Y%m%d')+'_hr_'+str(hour)
+        os.makedirs(direct)
+        os.makedirs(direct+"/GRIB2")
+        os.makedirs(direct+"/TIF")
 
-for i in fxx:
-    rainfall_data [i,0] = i
-    # Rename the file based on the info from above (e.g. 20170310_h00_f00_TMP_2_m_above_ground.grib2)
-    outfile_grib = direct + '/GRIB2/%s_h%02d_f%02d_%s.grib2' % (DATE.strftime('%Y%m%d'), hour, i,
-                                                                var_to_match.replace(':', '_').replace(' ', '_'))
-    outfile_tif = direct + '/TIF/%s_h%02d_f%02d_%s.tif' % (DATE.strftime('%Y%m%d'), hour, i,
-                                                           var_to_match.replace(':', '_').replace(' ', '_'))
-    outfile_tif_prj = direct + '/TIF/%s_h%02d_f%02d_%s_projected.tif' % (DATE.strftime('%Y%m%d'), hour, i,
-                                                                         var_to_match.replace(':', '_').replace(' ', '_'))
+        rainfall_data = np.zeros((19,8))
+        shp_filename = 'C:/Users/Ben Bowes/Documents/HRSD GIS/Shallow Wells/Norfolk_Wells_UTM.shp'
 
-    # Model file names are different than model directory names.
-    if model == 'hrrr':
-        model_dir = 'oper'
-    elif model == 'hrrrX':
-        model_dir = 'exp'
-    elif model == 'hrrrAK':
-        model_dir = 'alaska'
+        for i in fxx:
+            rainfall_data [i,0] = i
+            # Rename the file based on the info from above (e.g. 20170310_h00_f00_TMP_2_m_above_ground.grib2)
+            outfile_grib = direct + '/GRIB2/%s_h%02d_f%02d_%s.grib2' % (DATE.strftime('%Y%m%d'), hour, i,
+                                                                        var_to_match.replace(':', '_').replace(' ', '_'))
+            outfile_tif = direct + '/TIF/%s_h%02d_f%02d_%s.tif' % (DATE.strftime('%Y%m%d'), hour, i,
+                                                                   var_to_match.replace(':', '_').replace(' ', '_'))
+            outfile_tif_prj = direct + '/TIF/%s_h%02d_f%02d_%s_projected.tif' % (DATE.strftime('%Y%m%d'), hour, i,
+                                                                                 var_to_match.replace(':', '_').replace(' ', '_'))
 
-    # This is the URL to download the full GRIB2 file. We will use the cURL command
-    # to download the variable of interest from the byte range in step 3.
-    pandofile = 'https://pando-rgw01.chpc.utah.edu/%s/%s/%s/%s.t%02dz.wrf%sf%02d.grib2' \
-                % (model, field, DATE.strftime('%Y%m%d'), model, hour, field, i)
+            # Model file names are different than model directory names.
+            if model == 'hrrr':
+                model_dir = 'oper'
+            elif model == 'hrrrX':
+                model_dir = 'exp'
+            elif model == 'hrrrAK':
+                model_dir = 'alaska'
 
-    # This is the URL with the Grib2 file metadata. The metadata contains the byte
-    # range for each variable. We will identify the byte range in step 2.
-    sfile = pandofile+'.idx'
+            # This is the URL to download the full GRIB2 file. We will use the cURL command
+            # to download the variable of interest from the byte range in step 3.
+            pandofile = 'https://pando-rgw01.chpc.utah.edu/%s/%s/%s/%s.t%02dz.wrf%sf%02d.grib2' \
+                        % (model, field, DATE.strftime('%Y%m%d'), model, hour, field, i)
 
-    # 1) Open the Metadata URL and read the lines
-    request = urllib2.Request(sfile)
-    response = urllib2.urlopen(request, context = ssl._create_unverified_context())
-    # idxpage = urllib2.urlopen(sfile) #certificates are not validating correctly
-    lines = response.readlines()
+            # This is the URL with the Grib2 file metadata. The metadata contains the byte
+            # range for each variable. We will identify the byte range in step 2.
+            sfile = pandofile+'.idx'
 
-    # 2) Find the byte range for the variable. First find where the
-    #    variable is located. Keep a count (gcnt) so we can get the end
-    #    byte range from the next line.
-    gcnt = 0
-    for g in lines:
-        expr = re.compile(var_to_match)
-        if expr.search(g):
-            parts = g.split(':')
-            rangestart = parts[1]
-            parts = lines[gcnt+1].split(':')
-            rangeend = int(parts[1])-1
-            print 'range:', rangestart, rangeend
-            byte_range = str(rangestart) + '-' + str(rangeend)
+            # 1) Open the Metadata URL and read the lines
+            request = urllib2.Request(sfile)
+            response = urllib2.urlopen(request, context = ssl._create_unverified_context())
+            # idxpage = urllib2.urlopen(sfile) #certificates are not validating correctly
+            lines = response.readlines()
 
-            # 3) When the byte range is discovered, use cURL to download.
-            os.system('curl -s -o %s --range %s %s' % (outfile_grib, byte_range, pandofile))
-            os.system('curl -s -o %s --range %s %s' % (outfile_tif, byte_range, pandofile))
-            print 'downloaded', outfile_tif
+            # 2) Find the byte range for the variable. First find where the
+            #    variable is located. Keep a count (gcnt) so we can get the end
+            #    byte range from the next line.
+            gcnt = 0
+            for g in lines:
+                expr = re.compile(var_to_match)
+                if expr.search(g):
+                    parts = g.split(':')
+                    rangestart = parts[1]
+                    parts = lines[gcnt+1].split(':')
+                    rangeend = int(parts[1])-1
+                    print 'range:', rangestart, rangeend
+                    byte_range = str(rangestart) + '-' + str(rangeend)
 
-        gcnt += 1
+                    # 3) When the byte range is discovered, use cURL to download.
+                    os.system('curl -s -o %s --range %s %s' % (outfile_grib, byte_range, pandofile))
+                    os.system('curl -s -o %s --range %s %s' % (outfile_tif, byte_range, pandofile))
+                    print 'downloaded', outfile_tif
 
-
-# project hrrr data to same projection as wells and extract data for those points only
-    os.system('gdalwarp %s %s -t_srs "+proj=utm +zone=18 +datum=NAD83" -tr 500 500' % (outfile_tif, outfile_tif_prj))
-    # Open the TIF file
-    src_ds = gdal.Open(outfile_tif_prj)
-    if src_ds is None:
-        print "Failed to open the raster.\n"
-        sys.exit(1)
-    gt = src_ds.GetGeoTransform()
-    rb = src_ds.GetRasterBand(1)
-
-    # Open the shapefile
-    ds = ogr.Open(shp_filename, 1)
-    if ds is None:
-        print "Failed to open the study area well shapefile.\n"
-        sys.exit(1)
-    lyr = ds.GetLayerByName(splitext(basename(shp_filename))[0])
-    if lyr is None:
-        print "Error opening the shapefile layer"
-        sys.exit(1)
-
-    # Check that all 7 wells exist
-    if lyr.GetFeatureCount() != 7:
-        raise Exception(lyr.GetFeatureCount(), 'Not equal to number of wells'
-                                               ' in the study area, 7 wells')
-
-    # Extract the forecast rainfall data from the TIF file underneath each well
-    j = 1
-    for feat in lyr:
-        geom = feat.GetGeometryRef()
-        mx, my = geom.GetX(), geom.GetY()
-        # Convert from map to pixel coordinates. That takes less memory size and less computation time.
-        # Only works for geo-transforms with no rotation.
-        # x pixel
-        px = int((mx - gt[0]) / gt[1])
-        # y pixel
-        py = int((my - gt[3]) / gt[5])
-
-        # Using the Struct library to unpack the return value for each pixel
-        structval = rb.ReadRaster(px, py, 1, 1, buf_type=gdal.GDT_Float64)
-        pix_val = struct.unpack('d', structval)
-        rain_val = float(pix_val[0])
-        well_name = feat.GetField(feat.GetFieldIndex('MONITORING'))
-        print well_name, rain_val
-        rainfall_data[i,j] = rain_val*0.0393701
-        j += 1
+                gcnt += 1
 
 
-print rainfall_data
+            # project hrrr data to same projection as wells and extract data for those points only
+            os.system('gdalwarp %s %s -t_srs "+proj=utm +zone=18 +datum=NAD83" -tr 500 500' % (outfile_tif, outfile_tif_prj))
+            # Open the TIF file
+            src_ds = gdal.Open(outfile_tif_prj)
+            if src_ds is None:
+                print "Failed to open the raster.\n"
+                sys.exit(1)
+            gt = src_ds.GetGeoTransform()
+            rb = src_ds.GetRasterBand(1)
 
-np.savetxt(direct + '/rainfall.csv', rainfall_data, delimiter=',',
-           header='Forecast,MMPS-175,MMPS-129,MMPS-155,MMPS-043,MMPS-125,MMPS-170,MMPS-153')
+            # Open the shapefile
+            ds = ogr.Open(shp_filename, 1)
+            if ds is None:
+                print "Failed to open the study area well shapefile.\n"
+                sys.exit(1)
+            lyr = ds.GetLayerByName(splitext(basename(shp_filename))[0])
+            if lyr is None:
+                print "Error opening the shapefile layer"
+                sys.exit(1)
 
+            # Check that all 7 wells exist
+            if lyr.GetFeatureCount() != 7:
+                raise Exception(lyr.GetFeatureCount(), 'Not equal to number of wells'
+                                                       ' in the study area, 7 wells')
 
-# once rainfall is extracted, delete large grib and tif folders
-try:
-    shutil.rmtree(direct+"/GRIB2")
-    shutil.rmtree(direct+"/TIF")
-except OSError, e:
-    print ("Error: %s - %s." % (e.filename, e.strerror))
+            # Extract the forecast rainfall data from the TIF file underneath each well
+            j = 1
+            for feat in lyr:
+                geom = feat.GetGeometryRef()
+                mx, my = geom.GetX(), geom.GetY()
+                # Convert from map to pixel coordinates. That takes less memory size and less computation time.
+                # Only works for geo-transforms with no rotation.
+                # x pixel
+                px = int((mx - gt[0]) / gt[1])
+                # y pixel
+                py = int((my - gt[3]) / gt[5])
+
+                # Using the Struct library to unpack the return value for each pixel
+                structval = rb.ReadRaster(px, py, 1, 1, buf_type=gdal.GDT_Float64)
+                pix_val = struct.unpack('d', structval)
+                rain_val = float(pix_val[0])
+                well_name = feat.GetField(feat.GetFieldIndex('MONITORING'))
+                print well_name, rain_val
+                rainfall_data[i,j] = rain_val*0.0393701
+                j += 1
+
+        print rainfall_data
+
+        np.savetxt(direct + '/rainfall.csv', rainfall_data, delimiter=',',
+                   header='Forecast,MMPS-175,MMPS-129,MMPS-155,MMPS-043,MMPS-125,MMPS-170,MMPS-153')
+
+        # once rainfall is extracted, delete large grib and tif folders
+        try:
+            shutil.rmtree(direct+"/GRIB2")
+            shutil.rmtree(direct+"/TIF")
+        except OSError, e:
+            print("Error: %s - %s." % (e.filename, e.strerror))
