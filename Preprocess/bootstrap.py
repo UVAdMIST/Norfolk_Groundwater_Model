e3670dbcb9441794ce5a18a3d02acb2cb13b086d
bdb3m <bdb3m@virginia.edu>
2018-08-22 10:14:10 -0400

    update bootstrap, block size equal avg storm len

--- Preprocess/bootstrap.py
+++ Preprocess/bootstrap.py
@@ -1,15 +1,6 @@
 """
-This script creates 1000 bootstrap replicates of time series data and stores them as csv files.
-Columns to use for each well:
-
-MMPS    Columns
-043     [0, 1, 2, 7]
-125     [0, 1, 2, 3]
-129     [0, 1, 2, 3]
-153     [0, 1, 2, 7]
-155     [0, 1, 2, 5]
-170     [0, 1, 2, 3]
-175     [0, 1, 2, 3]
+This script creates 1000 bootstrap replicates of time series data for each well
+in Norfolk and stores them as csv files.
 """
 
 import pandas as pd
@@ -23,26 +14,52 @@ def bs_to_df(x):
     bs_df_list.append(df)
 
 
-# set base path to store results
-path = "C:/Users/Ben Bowes/PycharmProjects/Tensorflow/mmps175_bootstraps/"
+well_list = ["043", "125", "129", "153", "155", "170", "175"]
 
-# load dataset
-dataset_raw = pd.read_csv("C:/Users/Ben Bowes/Documents/HRSD GIS/Site Data/Data_2010_2018/MMPS_175_no_blanks_SI.csv",
-                          index_col=None, parse_dates=True, infer_datetime_format=True)
+for well in well_list:
+    # specify which columns and avg storm length to use for each well
+    if well == "043":
+        cols = [0, 1, 2, 7]
+        storm_avg = 83
+    if well == "125":
+        cols = [0, 1, 2, 3]
+        storm_avg = 82
+    if well == "129":
+        cols = [0, 1, 2, 3]
+        storm_avg = 137
+    if well == "153":
+        cols = [0, 1, 2, 7]
+        storm_avg = 89
+    if well == "155":
+        cols = [0, 1, 2, 5]
+        storm_avg = 91
+    if well == "170":
+        cols = [0, 1, 2, 3]
+        storm_avg = 120
+    if well == "175":
+        cols = [0, 1, 2, 3]
+        storm_avg = 132
 
-dataset_raw_np = np.array(dataset_raw)
-dataset_np = dataset_raw_np[:, [0, 1, 2, 3]]
+    # set base path to store results
+    path = "C:/Users/Ben Bowes/PycharmProjects/Tensorflow/mmps" + well + "_bootstraps/"
 
-# set up bootstrap parameters
-bootstrap = CircularBlockBootstrap(720, dataset_np)
+    # load dataset
+    dataset_raw = pd.read_csv("C:/Users/Ben Bowes/Documents/HRSD GIS/Site Data/Data_2010_2018/MMPS_" + well +
+                              "_no_blanks_SI.csv", index_col=None, parse_dates=True, infer_datetime_format=True)
 
-bs_df_list = []
-results = bootstrap.apply(bs_to_df, 1000)
+    dataset_raw_np = np.array(dataset_raw)
+    dataset_np = dataset_raw_np[:, cols]
 
-count = 0
-for bs in bs_df_list:
-    if count % 25 == 0:
-        print(count)
-    f = path + "bs" + str(count) + ".csv"
-    bs.to_csv(f)
-    count += 1
+    # set up bootstrap parameters
+    bootstrap = CircularBlockBootstrap(storm_avg, dataset_np)
+
+    bs_df_list = []
+    results = bootstrap.apply(bs_to_df, 1000)
+
+    count = 0
+    for bs in bs_df_list:
+        if count % 25 == 0:
+            print("well", well, "bootstrap:", count)
+        f = path + "bs" + str(count) + ".csv"
+        bs.to_csv(f)
+        count += 1
